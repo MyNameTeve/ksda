967e3cc4451e9d33522a6373abf3a049792f3e82
Jake Stephens <Jake@Jakes-MBP.wv.cc.cmu.edu>
2015-03-31 22:40:44 -0400

    Forum success message

--- ksda/kapsig/ksda/forms_forum.py
+++ ksda/kapsig/ksda/forms_forum.py
@@ -14,7 +14,7 @@ class PostForm(forms.Form):
 
 class ThreadForm(forms.Form):
     Title = forms.CharField(max_length=100, required=False)
-    Content = forms.CharField(max_length=1000, required=False)
+    Content = forms.CharField(max_length=1000, required=False, widget=forms.Textarea)
 
     # Customizes form validation for properties that apply to more
     # than one field.  Overrides the forms.Form.clean function.
@@ -22,3 +22,15 @@ class ThreadForm(forms.Form):
         # Calls our parent (forms.Form) .clean function, gets a dictionary
         # of cleaned data as a result
         cleaned_data = super(ThreadForm, self).clean()
+
+    def clean_Content(self):
+        content = self.cleaned_data['Content']
+        if not content:
+            raise forms.ValidationError('Please add content to your post')
+        return content
+
+    def clean_Title(self):
+        title = self.cleaned_data['Title']
+        if not title:
+            raise forms.ValidationError('Please add a title to your post')
+        return title
